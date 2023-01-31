6cf45fd29890fabe2aa72b98ba2173e7c141097f
TJ Aulner <tjaulner@gmail.com>
2023-01-30 20:19:52 -0600

    Initial

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,14 @@
+class PagesController < ApplicationController
+    def home 
+
+    end
+
+    def jobs
+        @listings = [
+            { company: "Stark Enterprises", position: "Ruby Developer", location: "St. Louis, MO" },
+            { company: "WayneEnterprises", position: "Ruby on Rails Engineer", location: "Kansas City, MO" },
+            { company: "OsCorp", position: "Full Stack Ruby Developer", location: "Springfield, MO" }, { company: "Hydra", position: "Ruby Engineer", location: "Columbia, MO" },
+            { company: "S.H.I.E.L.D.", position: "Ruby on Rails Developer", location: "St. Joseph, MO" },
+           ]
+    end
+end
